6cf56d85feae723ad0af340ff88631af8115218a
GameDevNoOne <zlati.volk@gmail.com>
2024-02-15 17:32:12 +0100

    Hahahahahahah. Let's just say that it's all working and move on

--- ShinobiGame/ShinobiStickMen/Assets/Scripts/Movement.cs
+++ ShinobiGame/ShinobiStickMen/Assets/Scripts/Movement.cs
@@ -7,6 +7,8 @@ public class Movement : MonoBehaviour
     //I should start having some good Documentation but oh well... I'll at least have some funny comments hopefully.
     public Rigidbody2D player;
     public float speed;
+    //A perfect solution. Believe me...
+    public float sprintSpeed;
     public Animator playerAnimator;
 
     // Start is called before the first frame update
@@ -21,10 +23,33 @@ public class Movement : MonoBehaviour
         Move();
     }
     //Breakcore hits hard right before realizing your speed needs to be in the thousands
+    //Okay not really thousands but you get my point
     public void Move()
     {
         float movement = Input.GetAxisRaw("Horizontal");
+        playerAnimator.SetFloat("Speed", movement);
 
-        player.velocity = new Vector2(movement * speed * Time.deltaTime, player.position.y);
+        //I'll regret this later but current me doesn't care
+        //3 minutes later I am regreting my decisions even more. I'm sure the if's will work absolutely fine without issue
+        //Obviously it doesn't work as it should. oh well the battery's really low so i gotta go :)
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerAnimator.SetBool("isSprinting", true);
+            player.velocity = new Vector2(movement * sprintSpeed * 100 * Time.deltaTime, player.position.y);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            playerAnimator.SetBool("isSprinting", false);
+            player.velocity = new Vector2(movement * speed * 100 * Time.deltaTime, player.position.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(movement * speed * 100 * Time.deltaTime, player.position.y);
+        }
+    }
+
+    public void Jump()
+    {
+
     }
 }
